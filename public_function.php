d87ee5bce1434db4678dac55548fe1383c7833fd
Liu Zhe <liuzhe@haohaninc.com>
2015-10-21 18:00:26 +0800

    modify user list

--- public_function.php
+++ public_function.php
@@ -18,7 +18,7 @@ function http_get($url) {
     $response = curl_exec($curl_instance);
     $status = curl_getinfo($curl_instance);
     curl_close($curl_instance);
-    
+
     if (intval($status["http_code"]) == 200) {
         return $response;
     } else {
@@ -26,10 +26,40 @@ function http_get($url) {
     }
 }
 
-function S($name, $obj){
+function http_post($url, $param, $post_file = false) {
+    $curl_instance = curl_init();
+    if (stripos($url, "https://") !== FALSE) {
+        curl_setopt($curl_instance, CURLOPT_SSL_VERIFYPEER, FALSE);
+        curl_setopt($curl_instance, CURLOPT_SSL_VERIFYHOST, FALSE);
+        curl_setopt($curl_instance, CURLOPT_SSLVERSION, 1); //CURL_SSLVERSION_TLSv1
+    }
+    if (is_string($param) || $post_file) {
+        $str_post = $param;
+    } else {
+        $aPOST = array();
+        foreach ($param as $key => $val) {
+            $aPOST[] = $key . "=" . urlencode($val);
+        }
+        $str_post = join("&", $aPOST);
+    }
+    curl_setopt($curl_instance, CURLOPT_URL, $url);
+    curl_setopt($curl_instance, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($curl_instance, CURLOPT_POST, true);
+    curl_setopt($curl_instance, CURLOPT_POSTFIELDS, $str_post);
+    $sContent = curl_exec($curl_instance);
+    $aStatus = curl_getinfo($curl_instance);
+    curl_close($curl_instance);
+    if (intval($aStatus["http_code"]) == 200) {
+        return $sContent;
+    } else {
+        return false;
+    }
+}
+
+function S($name, $obj) {
     file_put_contents($name, $obj, true);
 }
 
-function L($name){
+function L($name) {
     return file_get_contents($name);
 }
--- wechat_function.php
+++ wechat_function.php
@@ -7,11 +7,18 @@
  */
 include 'public_function.php';
 
+$send_url = 'https://api.weixin.qq.com/cgi-bin/message/custom/send?access_token=CJ0y80Lfrwu671hxSjZZS8Q6-zd6VYArHmu3yV2MCZjcCTYFxRKy0MblnBLXnR3i7HsbJ5O-i-8tnor7N2qggKMRfm7_FFpa_XF6loU-c6Q';
+$send_data = array('touser' => 'oUBaxs-nBvi86eeG_lbO3Q5o3Kps', 'msgtype' => 'text', 'text' => array('content' => '小染下午好吖'));
+$send_data = json_encode($send_data, JSON_UNESCAPED_UNICODE);
+print_r(http_post($send_url, $send_data));
+die;
+
 switch ($_GET['function']) {
     case 'get_user_num':
         get_user_num();
         break;
-    case 'extract_user_info':
+    case 'get_user_info':
+        get_user_info();
         break;
 
     default:
@@ -36,7 +43,7 @@ function get_user_num() {
     echo json_encode($ajax_response);
 }
 
-function extract_user_info(){
+function get_user_info(){
     $app_id = $_POST['app_id'];
     
     $ajax_response = array(
@@ -48,18 +55,21 @@ function extract_user_info(){
     }
     
     //获取关注者openid列表
-    
-    //
-    $url = 'https://api.weixin.qq.com/cgi-bin/user/get?access_token=' . L($app_id . '_access_token');
+    $url = 'https://api.weixin.qq.com/cgi-bin/user/get?access_token=' . L($app_id . '_access_token').'&next_openid=owogBtzPp97oFlUuhKTjPfO7jMq8';
     $ret_array = json_decode(http_get($url), true);
     $file_name = $app_id.'_user_openid_list';
-    write_array_to_file($file_name, $content_array, $file_name);
+    //write_array_to_file($file_name, $ret_array['data']['openid'], $file_name);
     
-    while ($ret_array['next_openid'] != ''){
-        
+    $ret_array['next_openid'] = 'owogBt8P7Ik7X_toVjFx0pdbd7ds';
+    
+    while ($ret_array['next_openid'] != '') {
+        $url = 'https://api.weixin.qq.com/cgi-bin/user/get?access_token=' . L($app_id . '_access_token') . '&next_openid=' . $ret_array['next_openid'];
+        $ret_array = json_decode(http_get($url), true);
+        print_r($ret_array);
+        die;
+        write_array_to_file($file_name, $ret_array['data']['openid'], $file_name);
     }
-    
-    
+
     $ajax_response['msg'] = 'get user amount successful';
     $ajax_response['amount'] = $ret_array['total'];
     echo json_encode($ajax_response);
